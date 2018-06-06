459a09c10d81748ee9b0efbbd465a1be9023b8a8
fabian <fabr2008@hotmail.com>
2018-06-06 10:18:11 -0500

    servicio firebase

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -7,11 +7,26 @@ import { Http } from '@angular/http';
 export class InformacionService {
   info:any ={};
   cargada:boolean = false;
+  cargada_sobre_nosotros:boolean = false;
+  equipo:any[] = [];
   constructor(public http:Http) {
+    this.carga_info();
+    this.carga_Nosotros();
+  }
+
+  public carga_info(){
     this.http.get("./assets/data/info.pagina.json").subscribe(data => {
-      console.log(data.json());
+      //console.log(data.json());
       this.info=data.json();
       this.cargada = true;
+    })
+  }
+
+  public carga_Nosotros(){
+    this.http.get("https://paginaweb-a6599.firebaseio.com/equipo.json").subscribe(data => {
+      console.log(data.json());
+      this.equipo=data.json();
+      this.cargada_sobre_nosotros = true;
     })
   }
 }
